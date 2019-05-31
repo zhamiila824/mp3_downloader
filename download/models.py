53d56ac407ee744f07d5e6345df40c942b49f05c
jamilya824 <jama.98.jk@gmail.com>
2019-05-31 19:08:00 +0600

    Template

--- download/models.py
+++ download/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Request(models.Model):
+    url = models.URLField(max_length=400)
+    email = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.url == None:
+            return "URL IS NULL"
+        return self.url
