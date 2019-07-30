7cf7b0fa164cac13a0841357bf3d17a0cab0eebc
jamilya824 <jama.98.jk@gmail.com>
2019-07-31 00:54:24 +0600

    Added postgresql

--- mp3_downloader/settings.py
+++ mp3_downloader/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'mp3_downloader.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
